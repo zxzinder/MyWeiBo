ac3abc4ef0113959d8cfd389e15085820269b032
zxzinder <macmini@bigdog.local>
2015-11-18 11:21:46 +0800

    Initial Commit

---
+++ MyWeiBo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyWeiBo
+//
+//  Created by MacMini on 15/11/18.
+//  Copyright © 2015年 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
